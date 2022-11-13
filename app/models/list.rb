c286f10f981dcbab889efdba16f0da8d8d2ddf99
corentin vanvyve <corentin.vanvyve@gmail.com>
2022-11-13 15:53:37 +0100

    cloudinary

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, presence: true, uniqueness: true, allow_blank: false
+  has_one_attached :photo
 end
